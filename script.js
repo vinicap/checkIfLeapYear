821dd8b6e8fe78aa72d85a3ea1758d41d88fa120
Vinicius <viniciusltds2019@gmail.com>
2020-10-28 11:29:50 -0300

    Finished model

---
+++ script.js
@@ -0,0 +1,13 @@
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
